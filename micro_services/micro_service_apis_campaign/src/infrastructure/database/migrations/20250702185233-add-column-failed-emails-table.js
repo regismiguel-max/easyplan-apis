8e1a4d3b70678d87fc93ffbe0d42c3f39755fb1d
Pedro Bueno Ferreira <pedro.bueno@easyplan.com.br>
2025-07-02 17:04:05 -0300

    feat: migrations

---
+++ micro_services/micro_service_apis_campaign/src/infrastructure/database/migrations/20250702185233-add-column-failed-emails-table.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    /**
+     * Add altering commands here.
+     *
+     * Example:
+     * await queryInterface.createTable('users', { id: Sequelize.INTEGER });
+     */
+    await queryInterface.addColumn(
+      'cliente_campanha_email_failed',
+      'reason',
+      {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    )
+  },
+
+  async down (queryInterface, Sequelize) {
+    /**
+     * Add reverting commands here.
+     *
+     * Example:
+     * await queryInterface.dropTable('users');
+     */
+    await queryInterface.removeColumn(
+      'cliente_campanha_email_failed',
+      'reason',
+    )
+  }
+};
